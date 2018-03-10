f47f81e601d2f52c6ee030f43b9ceaa9361b35a8
Douglas Hill <dsh0005@uah.edu>
2018-03-10 10:12:08 -0600

    Fix up listing's fields.

--- T4RSU/realestate/listing.py
+++ T4RSU/realestate/listing.py
@@ -12,24 +12,22 @@ class Listing(models.Model):
     Represents a listing.
     """
     listing_agent = models.ForeignKey('Agent', on_delete=models.PROTECT)
-    MLSNumber = models.IntegerField
+    MLSNumber = models.IntegerField()
     picture = models.ImageField()
-    price = models.FloatField
-    #Address?
-    squareFootage = models.FloatField
+    price = models.DecimalField(max_digits=13, decimal_places=2)    # 1 bn $ sale?
+    address = models.TextField()
+    squareFootage = models.FloatField()
     listing_agency = models.ForeignKey('Agency', on_delete=models.PROTECT)
-    description = models.TextField
-    roomDescription = models.TextField
-    subdivision = models.TextField #can be null
-    schoolDistrict = models.TextField
-    shopping = models.TextField
-    #schedule = models.ForeignKey('Showing', on_delete=models.PROTECT) Removed due to group discussion
-    totalHitCount = models.IntegerField
-    dailyHitCount = models.IntegerField
-    armCode = models.IntegerField
-    disarmCode = models.IntegerField
-    password = models.TextField
-    alarmNotes = models.TextField
-    isOcupied = models.BooleanField
-    lockBoxCode = models.IntegerField
-
+    description = models.TextField()
+    roomDescription = models.TextField()
+    subdivision = models.TextField(null=True)
+    schoolDistrict = models.TextField()
+    shopping = models.TextField()
+    totalHitCount = models.IntegerField()
+    dailyHitCount = models.IntegerField()
+    armCode = models.TextField(null=True)
+    disarmCode = models.TextField(null=True)
+    password = models.TextField(null=True)
+    alarmNotes = models.TextField()
+    isOccupied = models.BooleanField()
+    lockBoxCode = models.TextField()    # TODO: nullable?
