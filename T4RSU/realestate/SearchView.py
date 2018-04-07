c96b93c55b5765e5a8bd3f5868517fc044323316
Douglas Hill <dsh0005@uah.edu>
2018-04-07 15:08:02 -0500

    Add zip code field to listing and return queryset for SearchView.

--- T4RSU/realestate/SearchView.py
+++ T4RSU/realestate/SearchView.py
@@ -3,5 +3,27 @@
 
 from django.views.generic import ListView
 
+from . import Listing
+
 class SearchView(ListView):
     template_name = 'SearchView.html'
+    model = Listing
+
+    _filter_name_to_lookup = {
+        "area_min": "squareFootage__gte",
+        "area_max": "squareFootage__lte",
+        "price_min": "price__gte",
+        "price_max": "price__lte",
+        "zip": "zipCode",
+    }
+
+    def get_queryset(self):
+        get_dict = self.request.GET.dict()
+        q = Listing.objects.all()
+        # For every filter we understand...
+        for filter_name, lookup in SearchView._filter_name_to_lookup.items():
+            # ... if it's set...
+            if filter_name in get_dict:
+                # ... filter based on it.
+                q = q.filter(**{lookup: get_dict[filter_name]})
+        return q
