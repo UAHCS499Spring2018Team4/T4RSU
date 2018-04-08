156faa48fef43e2bf9f293432e761ffae6b6fc06
Douglas Hill <dsh0005@uah.edu>
2018-04-08 12:38:15 -0500

    Fix up the urls and MLSNumber a bit.

--- T4RSU/realestate/urls.py
+++ T4RSU/realestate/urls.py
@@ -4,11 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.OverviewView.as_view(), name='Overview'),
-
-    path('search/', views.SearchView, name='SearchView'),
-
-    re_path(r'^(?P<listing_id>[0-9]+)/del/$', views.ListingDeleteView, name='Delete'),
-
-    re_path(r'^(?P<listing_id>[0-9]+)/change/$', views.ListingEditView, name='ListChange'),
-
+    path('search/', views.SearchView.as_view(), name='SearchView'),
+    path('Listing/<int:MLSNumber>/', views.ListingView.as_view(), name='ListingView'),
+    path('Listing/<int:MLSNumber>/Delete/', views.ListingDeleteView.as_view(), name='Delete'),
+    path('Listing/<int:MLSNumber>/Edit/', views.ListingEditView.as_view(), name='ListChange'),
 ]
