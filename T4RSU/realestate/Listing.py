fbe9bd24a4d8c958793764d12f8472177e3cedec
Jonathan <jcs0042@uah.edu>
2018-04-10 22:54:04 -0500

    Required to commit before merge

--- T4RSU/realestate/Listing.py
+++ T4RSU/realestate/Listing.py
@@ -11,24 +11,24 @@ class Listing(models.Model):
     """
     #listing_agent = models.ForeignKey(Agent, on_delete=models.PROTECT)
     MLSNumber = models.IntegerField(unique=True)
-    #picture = models.ImageField()
-    #price = models.DecimalField(max_digits=13, decimal_places=2)    # 1 bn $ sale?
+    picture = models.ImageField()
+    price = models.DecimalField(max_digits=13, decimal_places=2)    # 1 bn $ sale?
     address = models.TextField()
-    #zipCode = models.IntegerField()
-    #squareFootage = models.FloatField()
+    zipCode = models.IntegerField()
+    squareFootage = models.FloatField()
     description = models.TextField()
-    #roomDescription = models.TextField()
-    #subdivision = models.TextField(null=True)
-    #schoolDistrict = models.TextField()
-    #shopping = models.TextField()
-    #totalHitCount = models.IntegerField()
-    #dailyHitCount = models.IntegerField()
-    #armCode = models.TextField(null=True)
-    #disarmCode = models.TextField(null=True)
-    #password = models.TextField(null=True)
-    #alarmNotes = models.TextField()
+    roomDescription = models.TextField()
+    subdivision = models.TextField(null=True)
+    schoolDistrict = models.TextField()
+    shopping = models.TextField()
+    totalHitCount = models.IntegerField()
+    dailyHitCount = models.IntegerField()
+    armCode = models.TextField(null=True)
+    disarmCode = models.TextField(null=True)
+    password = models.TextField(null=True)
+    alarmNotes = models.TextField()
     isOccupied = models.BooleanField()
-    #lockBoxCode = models.TextField()    # TODO: nullable?
+    lockBoxCode = models.TextField()    # TODO: nullable?
 
     def __str__(self):
         #return 'Number: ' + str(self.MLSNumber)
