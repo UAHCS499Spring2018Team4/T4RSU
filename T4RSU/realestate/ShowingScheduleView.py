9e140bdd56b2898fe3412f2b95d4c84af6d4012b
Douglas Hill <dsh0005@uah.edu>
2018-04-12 16:42:57 -0500

    Add listing to showing context.

--- T4RSU/realestate/ShowingScheduleView.py
+++ T4RSU/realestate/ShowingScheduleView.py
@@ -17,3 +17,8 @@ class ShowingScheduleView(ListView):
         shows = Showing.objects.filter(listing=lis)
         shows = shows.filter(start_time__gte=date.today())
         return shows
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['listing'] = Listing.objects.get(MLSNumber=self.kwargs['pk'])
+        return context
