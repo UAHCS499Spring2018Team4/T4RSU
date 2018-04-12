7de2fb789129c2b6b9f53a58ae774f46cae5a87c
Jonathan <jcs0042@uah.edu>
2018-04-12 15:58:38 -0500

    Merge remote-tracking branch 'remotes/origin/master' into dev-jonathan
    # Conflicts:
    #	T4RSU/realestate/agency.py
    #	T4RSU/realestate/models.py

--- T4RSU/realestate/ListingEditView.py
+++ T4RSU/realestate/ListingEditView.py
@@ -2,7 +2,32 @@
 # encoding=utf-8
 
 from django.views.generic import UpdateView
+from .Listing import Listing
+from django.shortcuts import render
 
 
 class ListingEditView(UpdateView):
-    template_name = 'ListingEdit.html'
+    template_name = 'ModifyListing.html'
+    model = Listing
+    fields = ['listing_agent',
+        'MLSNumber',
+        #'picture',
+        'price',
+        'address',
+        'zipCode',
+        'squareFootage',
+        'description',
+        'roomDescription',
+        'subdivision',
+        'schoolDistrict',
+        'shopping',
+        'armCode',
+        'disarmCode',
+        'password',
+        'alarmNotes',
+        'isOccupied',
+        'lockBoxCode',
+    ]
+
+    def form_invalid(self, form):
+        raise ValueError(str(form.errors))
