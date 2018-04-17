fdeeaee2f1eef1c79e6d5441ebc0ddf6c221ba77
Chris <cmm0032@uah.edu>
2018-04-17 16:23:22 -0500

    Updated ModifyListing.html to properly populate boxes

--- T4RSU/realestate/tasks.py
+++ T4RSU/realestate/tasks.py
@@ -7,7 +7,7 @@ from datetime import datetime
 logger = get_task_logger(__name__)
 
 
-@periodic_task(run_every=(crontab(hour="16", minute="03", day_of_week="*")))
+@periodic_task(run_every=(crontab(hour="16", minute="06", day_of_week="*")))
 def run_daily_emails():
     for entry in Listing.objects.all():
         entry.daily_hit_count()
