5a80f3d35ffa5a2b2224a210a72ae5ba4af91631
Douglas Hill <dsh0005@uah.edu>
2018-04-18 09:33:41 -0500

    Building up forms for 12 hours つ ◕_◕ ༽つ

--- T4RSU/realestate/ListingCreateView.py
+++ T4RSU/realestate/ListingCreateView.py
@@ -1,34 +1,66 @@
 #!/usr/bin/env python3
 # encoding=utf-8
 
+from django.forms import ModelForm
 from django.views.generic import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django import forms
 
 from .Listing import Listing
+from .photo import Photo
 
+class ListingCreateForm(ModelForm):
+    class Meta:
+        model = Listing
+        fields = ['listing_agent',
+            'MLSNumber',
+            'picture',
+            'price',
+            'address',
+            'zipCode',
+            'squareFootage',
+            'description',
+            'roomDescription',
+            'subdivision',
+            'schoolDistrict',
+            'shopping',
+            'armCode',
+            'disarmCode',
+            'password',
+            'alarmNotes',
+            'isOccupied',
+            'lockBoxCode',
+            'Picture1',
+            'Picture2',
+            'Picture3',
+            'Picture4',
+            'Picture5',
+        ]
+    Picture1 = forms.ImageField(required=False)
+    Picture2 = forms.ImageField(required=False)
+    Picture3 = forms.ImageField(required=False)
+    Picture4 = forms.ImageField(required=False)
+    Picture5 = forms.ImageField(required=False)
+
+    _picfieldnames = [
+        'Picture1',
+        'Picture2',
+        'Picture3',
+        'Picture4',
+        'Picture5',
+    ]
 
 class ListingCreateView(LoginRequiredMixin, CreateView):
     template_name = 'CreateListing.html'
     model = Listing
-    fields = ['listing_agent',
-        'MLSNumber',
-        'picture',
-        'price',
-        'address',
-        'zipCode',
-        'squareFootage',
-        'description',
-        'roomDescription',
-        'subdivision',
-        'schoolDistrict',
-        'shopping',
-        'armCode',
-        'disarmCode',
-        'password',
-        'alarmNotes',
-        'isOccupied',
-        'lockBoxCode',
-    ]
+    form_class = ListingCreateForm
+    Picture1 = forms.ImageField(required=False)
 
     def form_invalid(self, form):
         raise ValueError(str(form.errors))
+
+    def form_valid(self, form):
+        form.instance.save()
+        if 'Picture1' in form.cleaned_data:
+            Photo(listing=form.instance, picture=form.cleaned_data.get('Picture1')).save()
+        return super().form_valid(form)
--- T4RSU/realestate/ListingEditView.py
+++ T4RSU/realestate/ListingEditView.py
@@ -3,9 +3,10 @@
 
 from django.views.generic import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django import forms
 
 from .Listing import Listing
-
+from .photo import Photo
 
 class ListingEditView(LoginRequiredMixin, UpdateView):
     template_name = 'ModifyListing.html'
@@ -30,5 +31,14 @@ class ListingEditView(LoginRequiredMixin, UpdateView):
         'lockBoxCode',
     ]
 
+    Picture1 = forms.ImageField(required=False)
+
     def form_invalid(self, form):
         raise ValueError(str(form.errors))
+
+    def form_valid(self, form):
+        redir = super().form_valid(form)
+        raise ValueError(form.cleaned_data)
+        if form.cleaned_data['Picture1']:
+            Photo(listing=self.kwargs['pk'], picture=form.cleaned_data['Picture1']).save()
+        return redir
