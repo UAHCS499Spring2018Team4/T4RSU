3bed888f45f949dad02c1949ee12c8587cd46c96
Douglas Hill <dsh0005@uah.edu>
2018-04-18 12:08:24 -0500

    Listing editing can now add photos.

--- T4RSU/realestate/ListingEditView.py
+++ T4RSU/realestate/ListingEditView.py
@@ -3,42 +3,22 @@
 
 from django.views.generic import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django import forms
 
 from .Listing import Listing
 from .photo import Photo
+from .ListingForm import ListingForm
 
 class ListingEditView(LoginRequiredMixin, UpdateView):
     template_name = 'ModifyListing.html'
     model = Listing
-    fields = [#'listing_agent',
-        'MLSNumber',
-        'picture',
-        'price',
-        'address',
-        'zipCode',
-        'squareFootage',
-        'description',
-        'roomDescription',
-        'subdivision',
-        'schoolDistrict',
-        'shopping',
-        'armCode',
-        'disarmCode',
-        'password',
-        'alarmNotes',
-        'isOccupied',
-        'lockBoxCode',
-    ]
-
-    Picture1 = forms.ImageField(required=False)
+    form_class = ListingForm
 
     def form_invalid(self, form):
         raise ValueError(str(form.errors))
 
     def form_valid(self, form):
         redir = super().form_valid(form)
-        raise ValueError(form.cleaned_data)
-        if form.cleaned_data['Picture1']:
-            Photo(listing=self.kwargs['pk'], picture=form.cleaned_data['Picture1']).save()
+        for field in ListingForm.picfieldnames:
+            if field in form.cleaned_data and form.cleaned_data.get(field):
+                Photo(listing=form.instance, picture=form.cleaned_data.get(field)).save()
         return redir
